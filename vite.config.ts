281f717b8f74068a3c714525cbe27dc3d9b6dc5d
[dyad] <ahmedabdullah171299@gmail.com>
2026-03-18 13:59:31 +0200

    [dyad] Fixed the MIME type error by resetting the base path to '/' and updating asset links in index.html. - wrote 2 file(s)

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(() => ({
-  // Since your repository is 'lighthouse', we set the base to '/lighthouse/'
-  base: "/lighthouse/", 
+  base: "/", 
   server: {
     host: "::",
     port: 8080,
